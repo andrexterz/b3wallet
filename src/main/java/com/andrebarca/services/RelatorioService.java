741b8883b23fd387559a7e105707e7ae85f1ae0c
Andre Luiz F. R. Barca <andrexterz@gmail.com>
2019-01-29 14:31:07 -0200

    implementando relatorio dividendos

--- src/main/java/com/andrebarca/services/RelatorioService.java
+++ src/main/java/com/andrebarca/services/RelatorioService.java
@@ -1,17 +1,24 @@
 package com.andrebarca.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.andrebarca.models.Operacao;
-import com.andrebarca.repositories.OperacaoRepository;
-
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 /**
  *
  * @author andre
@@ -19,16 +26,20 @@ import com.andrebarca.repositories.OperacaoRepository;
 @RestController
 public class RelatorioService {
 
-    @Autowired
-    OperacaoRepository operacaoRepository;
+    @RequestMapping(value = "/api/dividendos/report", method=RequestMethod.POST)
+    public ResponseEntity<?> report(@RequestBody Map<String, Object> data) throws IOException {
+        data.forEach((k, v) -> {
+            System.out.println(k + " ---> " + v);
+        } );
+        //JsonDataSource datasource = new  JsonDataSource();
+        //
+        Map reportParameters = new HashMap();
+        File file = new ClassPathResource("reports/dividendos.jrxml").getFile();
+        JasperDesign jasperDesign = JRXmlLoader.load(file.getAbsolutePath());
+        JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+        //JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, reportParameters, datasource);
 
-    @RequestMapping(value = "/api/report", method=RequestMethod.GET)
-    public ResponseEntity<?> report() {
-        Iterable<Operacao> operacoes = this.operacaoRepository.findAll(
-                new Sort(Direction.ASC, "acao.codigo")
-                .and(new Sort(Direction.ASC,"dataOperacao"))
-                .and(new Sort(Direction.ASC,"tipoOperacao"))
-        );
-        return new ResponseEntity<>(operacoes, HttpStatus.OK);
+
+        return new ResponseEntity<>(file, HttpStatus.OK);
     }
 }
