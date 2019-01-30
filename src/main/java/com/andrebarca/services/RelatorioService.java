f66ce4f34426d31b6815674c3a17c0abf16fb1fd
andre <andrexterz@gmail.com>
2019-01-30 01:31:12 -0200

    implementando reports

--- src/main/java/com/andrebarca/services/RelatorioService.java
+++ src/main/java/com/andrebarca/services/RelatorioService.java
@@ -1,22 +1,25 @@
 package com.andrebarca.services;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JsonDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 /**
@@ -26,20 +29,22 @@ import net.sf.jasperreports.engine.xml.JRXmlLoader;
 @RestController
 public class RelatorioService {
 
-    @RequestMapping(value = "/api/dividendos/report", method=RequestMethod.POST)
-    public ResponseEntity<?> report(@RequestBody Map<String, Object> data) throws IOException {
-        data.forEach((k, v) -> {
-            System.out.println(k + " ---> " + v);
-        } );
-        //JsonDataSource datasource = new  JsonDataSource();
-        //
-        Map reportParameters = new HashMap();
+    @RequestMapping(value = "/api/json/report", method=RequestMethod.POST)
+    public ResponseEntity<?> report(InputStream data) throws IOException, JRException {
+    	String filename = "download";
+        JsonDataSource datasource = new JsonDataSource(data);
+        System.out.println(datasource.toString());
+        Map<String,Object> reportParameters = new HashMap<>();
+        reportParameters.put("PAGE_TITLE", "Exemplo Jasper PDF");
         File file = new ClassPathResource("reports/dividendos.jrxml").getFile();
         JasperDesign jasperDesign = JRXmlLoader.load(file.getAbsolutePath());
         JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-        //JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, reportParameters, datasource);
-
-
-        return new ResponseEntity<>(file, HttpStatus.OK);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, reportParameters, datasource);
+        byte[] content = JasperExportManager.exportReportToPdf(jasperPrint);
+        return ResponseEntity.ok()
+        		.header("Content-Disposition", String.format("attachment; filename=\"%s.pdf\"", filename))
+        		.contentLength(content.length)
+        		.contentType(MediaType.APPLICATION_PDF)
+        		.body(content);
     }
 }
